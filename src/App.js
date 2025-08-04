6b38b61128772a4157a13c970c014255e8f04a27
Kumthon <itznongdong@gmail.com>
2025-08-04 10:32:46 +0700

    ParameterInEventdata
    ParameterInEventdata

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import {Header,Content,Footer} from './func-components'
 import {Button} from "./class-components";
+import {Calculator2} from "./calculator";
 
 function App() {
   return (
     [
     <Header/>,
+    <p><center><Calculator2/></center></p>,
     <p><center><Button/></center></p>,
     <Content/>,
     <Footer/>
