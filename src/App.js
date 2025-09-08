abb5f07040056edde03e9e3c4ff4fa0bf41f65da
Kumthon <itznongdong@gmail.com>
2025-09-08 10:07:29 +0700

    router2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React,{ createContext,useState} from "react"
-import Menu from "./component/Menu";
-import Quiz from "./component/Quiz";
-import Score from "./component/Score";
-
-import './App.css'
+import Router2 from "./router2";
 
 export const DataContext = createContext()
 
 function App() {
-  const [appState,setAppState] = useState("menu")
-  const [score,setScore] = useState(0)
   return (
-    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
-      <div className="App">
-        <h1>Web Development Quiz</h1>
-        {appState === "menu" && <Menu/>}
-        {appState === "quiz" && <Quiz/>}
-        {appState === "score" && <Score/>}
-      </div>
-    </DataContext.Provider>
-    
+    <Router2/>
   );
 }
 
